36fdc039786fea803ffb53fd6e174add8ebdbf4c
KeanCaballero <keancaballero147@gmail.com>
2026-05-15 23:34:35 +0800

    Add 5s timeout to refresh token on boot to prevent white screen

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,9 +1,10 @@
 import { AccountService } from '@app/_services';
-import { catchError } from 'rxjs/operators';
+import { catchError, timeout } from 'rxjs/operators';
 import { EMPTY } from 'rxjs';
 
 export function appInitializer(accountService: AccountService) {
     return () => accountService.refreshToken().pipe(
+        timeout(5000),         // give up after 5 seconds
         catchError(() => EMPTY)
     );
 }
